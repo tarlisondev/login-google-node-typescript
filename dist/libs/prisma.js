8bf9792f65b75c6906bbe8ac045f5a02f1420b4a
unknown <tarlison.dev@gmail.com>
2025-06-04 20:15:51 -0300

    update in project

--- dist/libs/prisma.js
+++ dist/libs/prisma.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const prisma_1 = require("../generated/prisma");
+const client_1 = require("@prisma/client");
 const globalForPrisma = globalThis;
-exports.prisma = globalForPrisma.prisma || new prisma_1.PrismaClient();
+exports.prisma = globalForPrisma.prisma || new client_1.PrismaClient();
 if (process.env.NODE_ENV !== 'production')
     globalForPrisma.prisma = exports.prisma;
